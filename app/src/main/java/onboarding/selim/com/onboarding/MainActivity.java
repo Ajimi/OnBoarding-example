b082bc727645a81412ac04d21e0af3ed26a501e0
Selim Ajimi <sloumma.21@gmail.com>
2017-03-21 12:19:05 +0100

    Update and extracting string in MainActivity

--- app/src/main/java/onboarding/selim/com/onboarding/MainActivity.java
+++ app/src/main/java/onboarding/selim/com/onboarding/MainActivity.java
@@ -14,11 +14,12 @@ public class MainActivity extends AppIntro {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addSlide(AppIntroFragment.newInstance("Explore Products", "Explore thousands of products and start shoping", R.drawable.ic_explore_poduct, Color.parseColor("#ffffff"), Color.parseColor("#000001"), Color.parseColor("#000001")));
-        addSlide(AppIntroFragment.newInstance("Make Payments", "Up to ten plus options of payments", R.drawable.ic_spend_money, Color.parseColor("#ffffff"), Color.parseColor("#000001"), Color.parseColor("#000001")));
-        addSlide(AppIntroFragment.newInstance("Enjoy shopping", "Get high quality products with reasonable prices", R.drawable.ic_enjoy_shopping, Color.parseColor("#ffffff"), Color.parseColor("#000001"), Color.parseColor("#000001")));
-        setProgressButtonEnabled(true);
+        addSlide(AppIntroFragment.newInstance(getString(R.string.explore_products), getString(R.string.explore_products_description), R.drawable.ic_explore_poduct, Color.parseColor("#ffffff"), Color.parseColor("#000001"), Color.parseColor("#000001")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.make_payments), getString(R.string.make_payments_description), R.drawable.ic_spend_money, Color.parseColor("#ffffff"), Color.parseColor("#000001"), Color.parseColor("#000001")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.enjoy_shopping), getString(R.string.enjoy_shopping_description), R.drawable.ic_enjoy_shopping, Color.parseColor("#ffffff"), Color.parseColor("#000001"), Color.parseColor("#000001")));
 
+        setProgressButtonEnabled(true);
+        
         setColorDoneText(Color.parseColor("#000000"));
         setColorSkipButton(Color.parseColor("#000000"));
         setIndicatorColor(Color.parseColor("#22222222"), Color.parseColor("#ffAf8f"));
